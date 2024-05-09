3f829da7c85b9176387a72d8ab16150e039261a6
RaduBreazu <radumihaibreazu@gmail.com>
2024-05-09 14:52:24 +0300

    Implementing interpreter integration

--- Dockerfile
+++ Dockerfile
@@ -2,8 +2,9 @@ FROM haskell:9.6.4
 WORKDIR /app
 RUN cabal update && cabal install --lib containers-0.6.7
 RUN cabal install --lib scotty
-RUN cabal install --lib http-types
-RUN cabal install --lib aeson
+RUN cabal install --lib http-types-0.12.4
+RUN cabal install --lib aeson-2.2.1.0
+RUN cabal install --lib text
 COPY Main.hs ./Main.hs
 COPY Interpreter.hs ./Interpreter.hs
 COPY Stack.hs ./Stack.hs
--- Main.hs
+++ Main.hs
@@ -4,7 +4,7 @@ module Main where
 
 import Interpreter
 
-import Data.Text.Lazy as Text (Text, pack)
+import Data.Text.Lazy as Text (Text, pack, unpack)
 import System.IO
 import System.Environment
 import Control.Applicative
@@ -13,6 +13,12 @@ import Data.Aeson
 import Data.Aeson.Types (Parser)
 import Web.Scotty
 
+newtype Response = StringResp {message :: String} deriving (Show)
+
+instance ToJSON Response where
+    toJSON :: Response -> Value
+    toJSON (StringResp msg) = object ["error" .= msg]
+
 newtype Expression = Expression {getExpr :: String} deriving (Show)
 
 instance FromJSON Expression where
@@ -20,13 +26,22 @@ instance FromJSON Expression where
     parseJSON = withObject "Expression" $ \v -> Expression
         <$> v .: "expr"
 
-instance ToJSON Expression where
-    toJSON :: Expression -> Value
-    toJSON (Expression expr) = object ["expr" .= expr]
+instance Parsable Expression where
+    parseParam :: Text -> Either Text Expression
+    parseParam = Right . Expression . unpack
+
+instance ToJSON LambdaExpr where
+    toJSON :: LambdaExpr -> Value
+    toJSON expr = object ["expr" .= show expr]
 
 main :: IO ()
 main = do
     scotty 8000 $ do
         put "/interpreter" $ do
-            input <- jsonData :: ActionM Expression
-            json $ Expression $ show $ evalLambdaExpr (getExpr input)
+            authHeader <- header "Authorization"
+            case authHeader of
+                Nothing -> do
+                    text "Unauthorized access"
+                Just _ -> do
+                    input <- formParam "code"
+                    text $ pack $ show $ evalLambdaExpr (getExpr input)
