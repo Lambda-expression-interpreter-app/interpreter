adf453962c50787aa9518d3995ff79786f0c0433
RaduBreazu <radumihaibreazu@gmail.com>
2024-05-09 20:58:46 +0300

    Implemented interpreter integration

--- Dockerfile
+++ Dockerfile
@@ -2,9 +2,8 @@ FROM haskell:9.6.4
 WORKDIR /app
 RUN cabal update && cabal install --lib containers-0.6.7
 RUN cabal install --lib scotty
-RUN cabal install --lib http-types-0.12.4
-RUN cabal install --lib aeson-2.2.1.0
 RUN cabal install --lib text
+RUN cabal install --lib utf8-string-1.0.2
 COPY Main.hs ./Main.hs
 COPY Interpreter.hs ./Interpreter.hs
 COPY Stack.hs ./Stack.hs
--- Main.hs
+++ Main.hs
@@ -2,46 +2,25 @@
 
 module Main where
 
+-- import Debug.Trace
 import Interpreter
 
 import Data.Text.Lazy as Text (Text, pack, unpack)
+import qualified Data.ByteString.Lazy.UTF8 as BL
 import System.IO
 import System.Environment
 import Control.Applicative
 
-import Data.Aeson
-import Data.Aeson.Types (Parser)
 import Web.Scotty
-
-newtype Response = StringResp {message :: String} deriving (Show)
-
-instance ToJSON Response where
-    toJSON :: Response -> Value
-    toJSON (StringResp msg) = object ["error" .= msg]
-
-newtype Expression = Expression {getExpr :: String} deriving (Show)
-
-instance FromJSON Expression where
-    parseJSON :: Value -> Parser Expression
-    parseJSON = withObject "Expression" $ \v -> Expression
-        <$> v .: "expr"
-
-instance Parsable Expression where
-    parseParam :: Text -> Either Text Expression
-    parseParam = Right . Expression . unpack
-
-instance ToJSON LambdaExpr where
-    toJSON :: LambdaExpr -> Value
-    toJSON expr = object ["expr" .= show expr]
 
 main :: IO ()
 main = do
     scotty 8000 $ do
-        put "/interpreter" $ do
+        post "/interpreter" $ do
             authHeader <- header "Authorization"
             case authHeader of
                 Nothing -> do
                     text "Unauthorized access"
                 Just _ -> do
-                    input <- formParam "code"
-                    text $ pack $ show $ evalLambdaExpr (getExpr input)
+                    input <- body
+                    text $ pack $ show $ evalLambdaExpr (BL.toString input)
